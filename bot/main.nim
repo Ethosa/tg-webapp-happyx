a6d5818e29b4dc56d5925e012ecc7e71523cae9d
Ethosa <social.ethosa@gmail.com>
2024-06-29 15:10:47 +0700

    update

--- bot/main.nim
+++ bot/main.nim
@@ -10,7 +10,7 @@ proc updateHandler(b: Telebot, u: Update): Future[bool] {.gcsafe, async.} =
   var response = u.message
   if response.text.len > 0:
     let text = response.text
-    var google = KeyboardButton(text: "Search the web", webApp: WebAppInfo(url: "https://quick-loops-prove-116-110-41-44.loca.lt/webapp.html"))
+    var google = KeyboardButton(text: "Search the web", webApp: WebAppInfo(url: "https://ethosa.github.io/tg-webapp-happyx/#/"))
 
     let replyMarkup = ReplyKeyboardMarkup(kind: kReplyKeyboardMarkup, keyboard: @[@[google]])
 
