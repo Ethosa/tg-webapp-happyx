5b489ac6c8f1d804348bf5c0e5480be51443e31d
Ethosa <social.ethosa@gmail.com>
2024-07-01 16:39:43 +0700

    upd

--- client/src/main.nim
+++ client/src/main.nim
@@ -3,12 +3,18 @@ import happyx
 import telebot/webapp
 
 
-var
-  tg = Telegram.WebApp
-  themeParams = tg.themeParams
-  bgColor = themeParams.bg_color
-  textColor = themeParams.text_color
+var tg = Telegram.WebApp
 
+echo tg[]
+
+var counter = remember 0
+
+discard tg.MainButton.setText("Click me!")
+discard tg.MainButton.show()
+discard tg.MainButton.onClick(proc() =
+  counter->inc()
+)
+tg.expand()
 
 # Declare application with ID "app"
 appRoutes("app"):
@@ -16,13 +22,17 @@ appRoutes("app"):
   "/{p:string}":
     tDiv(
       class = "flex flex-col w-screen min-h-screen h-full p-12",
-      style = fmt"background: {bgColor}; color: {textColor}"
+      style = fmt"background: {tg.themeParams.bgColor}; color: {tg.themeParams.textColor}"
     ):
       tP:
-        "Hello from Nim!"
+        "Hello from Nim! Counter is "
+        {counter}
+      tButton(
+        style = fmt"background: {tg.themeParams.button_color}; color: {tg.themeParams.button_text_color}"
+      ):
+        "click me!"
+        @click:
+          counter->inc()
       tP:
-        {window.location.href}
-      tP:
-        {p}
-      tP:
+        "Telegram v"
         {tg.version}
