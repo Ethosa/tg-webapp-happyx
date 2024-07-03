ab0febbcf78a84565775b4ed5ddb83a1b0ac8346
Ethosa <social.ethosa@gmail.com>
2024-07-03 20:06:30 +0700

    add comments

--- bot/main.nim
+++ bot/main.nim
@@ -5,12 +5,15 @@ import
   std/strutils
 
 
+# Получаем API ключ для работы с ботом
 const API_KEY = slurp("secret.key").strip()
 
 
+# Обработчик всех входящих событий
 proc updateHandler(b: Telebot, u: Update): Future[bool] {.gcsafe, async.} =
   var response = u.message
   if response.text.len > 0:
+    # Отсылаем обратно сообщение с единственной кнопкой
     discard await b.sendMessage(
       response.chat.id, "Play in this now!",
       replyMarkup = InlineKeyboardMarkup(
@@ -23,11 +26,18 @@ proc updateHandler(b: Telebot, u: Update): Future[bool] {.gcsafe, async.} =
     )
 
 
+# Блок ниже скомпилируется только в том случае,
+# если мы запускаем этот файл напрямую.
+#
+# аналог питоновского if __name__ == '__main__'
 when isMainModule:
+  # Подрубаем логирование
   var L = newConsoleLogger()
   addHandler(L)
 
+  # Создаем экземпляр бота
   let bot = newTeleBot(API_KEY)
 
+  # Запускаем слушатель
   bot.onUpdate(updateHandler)
   bot.poll(timeout=300, clean = true)
