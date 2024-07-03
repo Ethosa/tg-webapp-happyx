6d7c27cc2dbc1fac30ac3e40bb614433a4309461
Ethosa <social.ethosa@gmail.com>
2024-07-03 19:21:02 +0700

    update

--- client/src/main.nim
+++ client/src/main.nim
@@ -25,13 +25,17 @@ appRoutes "app":
   # Основная страница
   "/{p:string}":
     tDiv(
-      class = fmt"flex flex-col w-screen min-h-screen h-full p-12 bg-[{tg.themeParams.bgColor}] text-[{tg.themeParams.textColor}]",
+      class = fmt"flex flex-col justify-content items-center w-screen min-h-screen h-screen p-12 bg-[{tg.themeParams.bgColor}] text-[{tg.themeParams.textColor}]",
     ):
-      tP(class = "text-3xl"):
-        "Hello from Nim!"
-      tP(class = "text-7xl"):
-        {counter}
-      tDiv(class = "fixed bottom-4 right-4 opacity-70"):
-        tP:
+      tDiv(class = "w-full h-full flex flex-col items-center justify-center"):
+        tP(class = "text-3xl"):
+          "Hello from Nim, "
+          tSpan(class = fmt"text-[{tg.themeParams.buttonColor}]"):
+            {tg.initDataUnsafe.user.first_name}
+          "!"
+        tP(class = "text-9xl"):
+          {counter}
+      tDiv(class = "fixed bottom-32 right-8 opacity-70"):
+        tP(class = "text-3xl"):
           "Your telegram version is v"
           {tg.version}
